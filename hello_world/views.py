a60eceffd4bd02fde00e84b6bfeb5fb8bd91ed49
unknown <admstud@MX1-Docker-24.EDU-WSB.local>
2024-04-11 10:34:42 +0200

    try to remove error

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = ""Roman"
+moje_imie = "Roman"
 msg = "Hi world"
 
 @app.route('/')
